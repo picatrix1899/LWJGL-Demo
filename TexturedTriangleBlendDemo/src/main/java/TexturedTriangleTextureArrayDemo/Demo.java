c8f755df32dc8a235842449cf421ff406296e1b6
picatrix1899 <FlorianZilkenat@gmx.de>
2021-01-09 02:12:26 +0100

    Created a demo for blending.

---
+++ TexturedTriangleBlendDemo/src/main/java/TexturedTriangleTextureArrayDemo/Demo.java
@@ -0,0 +1,399 @@
+package TexturedTriangleTextureArrayDemo;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.FloatBuffer;
+
+import javax.imageio.ImageIO;
+
+import org.lwjgl.glfw.GLFW;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.opengl.GL;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL13;
+import org.lwjgl.opengl.GL15;
+import org.lwjgl.opengl.GL20;
+import org.lwjgl.opengl.GL30;
+import org.lwjgl.system.MemoryStack;
+import org.lwjgl.system.MemoryUtil;
+
+public class Demo
+{
+	// The initial width of the window.
+	public static int WIDTH = 800;
+	
+	// The initial height of the window.
+	public static int HEIGHT = 600;
+	
+	public static void main(String[] args) { new Demo().run(); }
+
+	public void run()
+	{
+		/* ============
+		 *  SETUP CODE
+		 * ============ */
+		
+		/* ------------
+		 *  GLFW Setup
+		 * ------------ */
+		
+		// Creating an error callback to print occuring errors with GLFW to the console.
+		GLFWErrorCallback errorCallback = GLFWErrorCallback.createPrint(System.out);
+		
+		// Initializing GLFW.
+		GLFW.glfwInit();
+		
+		// Setting the error callback.
+		GLFW.glfwSetErrorCallback(errorCallback);
+		
+		/* --------------
+		 *  Window Setup
+		 * -------------- */
+		
+		// Defining important properties for the OpenGL context of the window.
+		
+		// Defining the minimum OpenGL version required to run.
+		GLFW.glfwWindowHint(GLFW.GLFW_CONTEXT_VERSION_MAJOR, 4);
+		GLFW.glfwWindowHint(GLFW.GLFW_CONTEXT_VERSION_MINOR, 2);
+		
+		// Defining the OpenGL profile. This is usually "CORE".
+		GLFW.glfwWindowHint(GLFW.GLFW_OPENGL_PROFILE, GLFW.GLFW_OPENGL_CORE_PROFILE);
+		
+		// Creating the window with the initial width and height and the title "Demo".
+		long windowId = GLFW.glfwCreateWindow(WIDTH, HEIGHT, "Demo", 0, 0);
+		
+		// If the window id or window handle is "0" the creation of the window failed.
+		if(windowId == 0)
+		{
+			System.err.println("Cannot create window.");
+			System.exit(-1);
+		}
+			
+		// Setting the OpenGL Context (contains all the resources like textures, shaders etc.) of the window as current to use.
+		GLFW.glfwMakeContextCurrent(windowId);
+
+		// Creating the OpenGL capabilities for this window.
+		GL.createCapabilities();
+		
+		/* ===========
+		 *  INIT CODE
+		 * =========== */
+		
+		// Generate the id for the virtual object called vertex array object (VAO) representing our triangle.
+		// The VAO holds references to buffers containing vertex data like positions, texture coordinates, normals etc. but also
+		// pointers to these informations, that will become important for seperatly sending the data to the vertex shader.
+		int triangleVAOId = GL30.glGenVertexArrays();
+		
+		// Binding the VAO for setup.
+		GL30.glBindVertexArray(triangleVAOId);
+
+		// Generate an id for a buffer called vertex buffer object (VBO) that will be referenced by the VAO.
+		// It will contain the vertex data.
+		int triangleVertexVBOId = GL15.glGenBuffers();
+		
+		// Bind the VBO for setup.
+		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, triangleVertexVBOId);
+		
+		try(MemoryStack stack = MemoryStack.stackPush())
+		{
+			// Allocating a temporary float buffer and filling it with vertex data.
+			// Here the data for a single vertex consists of 2 floats for position x, y and 2 floats for texture coordinates u and v.
+			FloatBuffer vertexBuffer = stack.mallocFloat(3 * 4);
+			vertexBuffer.put(new float[] {
+				-0.5f, -0.5f, 0.0f, 0.0f,
+				0.5f, -0.5f, 1.0f, 0.0f,
+				0.0f,  0.5f, 0.5f, 1.0f});
+			
+			// Reset the insert/read position to 0 to allow reading from the beginning of the buffer.
+			vertexBuffer.flip();
+			
+			// Put the vertex data safed in the temporary buffer into the VBO and telling
+			// the VBO, that the buffered data will be "static" and therefore doesn't change.
+			GL15.glBufferData(GL15.GL_ARRAY_BUFFER, vertexBuffer, GL15.GL_STATIC_DRAW);
+		}
+		
+		// Setting the pointer for position x, y in vertex data.
+		// The stride is the amount of values per vertex in bytes.
+		// In this case it's four float values á four bytes.
+		GL20.glVertexAttribPointer(0, 2, GL11.GL_FLOAT, false, 0, 0);
+		
+		// Setting the pointer for texture coordinates u and v in vertex data.
+		// The stride is the amount of values per vertex in bytes.
+		// In this case it's four float values á four bytes.
+		// The pointer aka the offset is the amount of values before the current value range in bytes.
+		// In this case the previous values where the positions consisting of two floats á four bytes.
+		GL20.glVertexAttribPointer(1, 2, GL11.GL_FLOAT, false, 0, 0);
+
+		// Generate an id for the shader program.
+		int shaderProgramId = GL20.glCreateProgram();
+		
+		// Generate an id for the vertex shader.
+		int vertexShaderId = GL20.glCreateShader(GL20.GL_VERTEX_SHADER);
+
+		// Pass vertex shader source to the shader.
+		GL20.glShaderSource(vertexShaderId, ""
+			+ "#version 400\n"
+			+ "in vec2 coords;\n"
+			+ "in vec2 texCoords;\n"
+			+ "out vec2 pass_texCoords;\n"
+			+ "void main()\n"
+			+ "{\n"
+			+ "	gl_Position = vec4(vec3(coords, 1.0), 1.0);\n"
+			+ "	pass_texCoords = texCoords;\n"
+			+ "}"
+			);
+		
+		// Compile the vertex shader and check for compilation errors.
+		GL20.glCompileShader(vertexShaderId);
+		if(GL20.glGetShaderi(vertexShaderId, GL20.GL_COMPILE_STATUS) == GL11.GL_FALSE)
+		{
+			System.err.println("Vertex shader compilation error.");
+			System.err.println(GL20.glGetShaderInfoLog(vertexShaderId, 1000));
+			System.exit(-1);
+		}
+		
+		// Generate an id for the fragment shader.
+		int fragmentShaderId = GL20.glCreateShader(GL20.GL_FRAGMENT_SHADER);
+		
+		// Pass fragment shader source to the shader.
+		GL20.glShaderSource(fragmentShaderId, ""
+			+ "#version 400\n"
+			+ "in vec2 pass_texCoords;\n"
+			+ "layout(location=0) out vec4 out_Color;\n"
+			+ "uniform sampler2D diffuse;\n"
+			+ "void main()\n"
+			+ "{\n"
+			+ "	out_Color = texture(diffuse, pass_texCoords);\n"
+			+ "}"
+			);
+
+		// Compile the fragment shader and check for compilation errors.
+		GL20.glCompileShader(fragmentShaderId);
+		if(GL20.glGetShaderi(fragmentShaderId, GL20.GL_COMPILE_STATUS) == GL11.GL_FALSE)
+		{
+			System.err.println(GL20.glGetShaderInfoLog(fragmentShaderId, 1000));
+			System.exit(-1);
+		}
+		
+		// Attach the vertex and fragment shader to the shader program.
+		GL20.glAttachShader(shaderProgramId, vertexShaderId);
+		GL20.glAttachShader(shaderProgramId, fragmentShaderId);
+		
+		// Bind the VAO pointer for vertex positions to the shader input variable "coords".
+		GL20.glBindAttribLocation(shaderProgramId, 0, "coords");
+		
+		// Bind the VAO pointer for vertex texture coordinates to the shader input variable "texCoords".
+		GL20.glBindAttribLocation(shaderProgramId, 1, "texCoords");
+		
+		// Link the shader program together and check for linking errors.
+		GL20.glLinkProgram(shaderProgramId);
+		if(GL20.glGetProgrami(shaderProgramId, GL20.GL_LINK_STATUS) == GL11.GL_FALSE)
+		{
+			System.err.println("Shader linking error.");
+			System.err.println(GL20.glGetProgramInfoLog(vertexShaderId, 1000));
+			System.exit(-1);
+		}
+		
+		// Validate the shader program and check for validation errors.
+		GL20.glValidateProgram(shaderProgramId);
+		if(GL20.glGetProgrami(shaderProgramId, GL20.GL_VALIDATE_STATUS) == GL11.GL_FALSE)
+		{
+			System.err.println("Shader validation error.");
+			System.err.println(GL20.glGetProgramInfoLog(vertexShaderId, 1000));
+			System.exit(-1);
+		}
+		
+		// Retrieve the locations for the uniform variables in the shader program to set their values later.
+		int textureUniformLocation = GL20.glGetUniformLocation(shaderProgramId, "diffuse");
+
+		BufferedImage texture1 = null;
+		try
+		{
+			texture1 = ImageIO.read(new File("test1.png"));
+		} catch (IOException e)
+		{
+			e.printStackTrace();
+			System.exit(-1);
+		}
+
+		int texture1Width = texture1.getWidth();
+		int texture1Height = texture1.getHeight();
+		
+		int[] pixels1 = new int[texture1Width * texture1Height];
+		texture1.getRGB(0, 0, texture1Width, texture1Height, pixels1, 0, texture1Width);
+
+		int textureId1 = GL11.glGenTextures();
+		
+		GL11.glBindTexture(GL11.GL_TEXTURE_2D, textureId1);
+
+		GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR);
+		GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_LINEAR);
+
+		ByteBuffer buffer1 = MemoryUtil.memAlloc(texture1Width * texture1Height * 4);
+
+		for(int y = 0; y < texture1Height; y++)
+			for(int x = 0; x < texture1Width; x++)
+			{
+				int pixel = pixels1[y * texture1Width + x];
+				
+				buffer1.put((byte)((pixel >> 16) & 0xFF)); // Red component
+				buffer1.put((byte)((pixel >> 8) & 0xFF)); // Green component
+				buffer1.put((byte)(pixel & 0xFF)); // Blue component
+				buffer1.put((byte)((pixel >> 24) & 0xFF)); //Alpha component
+			}
+
+		buffer1.flip();
+		
+		GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGBA8, texture1Width, texture1Height, 0, GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, buffer1);
+		
+		MemoryUtil.memFree(buffer1);
+
+		BufferedImage texture2 = null;
+		try
+		{
+			texture2 = ImageIO.read(new File("test2.png"));
+		} catch (IOException e)
+		{
+			e.printStackTrace();
+			System.exit(-1);
+		}
+
+		int texture2Width = texture2.getWidth();
+		int texture2Height = texture2.getHeight();
+		
+		int[] pixels2 = new int[texture2Width * texture2Height];
+		texture2.getRGB(0, 0, texture2Width, texture2Height, pixels2, 0, texture2Width);
+
+		int textureId2 = GL11.glGenTextures();
+		
+		GL11.glBindTexture(GL11.GL_TEXTURE_2D, textureId2);
+
+		GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR);
+		GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_LINEAR);
+
+		ByteBuffer buffer2 = MemoryUtil.memAlloc(texture2Width * texture2Height * 4);
+
+		for(int y = 0; y < texture2Height; y++)
+			for(int x = 0; x < texture2Width; x++)
+			{
+				int pixel = pixels2[y * texture2Width + x];
+				
+				buffer2.put((byte)((pixel >> 16) & 0xFF)); // Red component
+				buffer2.put((byte)((pixel >> 8) & 0xFF)); // Green component
+				buffer2.put((byte)(pixel & 0xFF)); // Blue component
+				buffer2.put((byte)((pixel >> 24) & 0xFF)); //Alpha component
+			}
+
+		buffer2.flip();
+		
+		GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGBA8, texture2Width, texture2Height, 0, GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, buffer2);
+		
+		MemoryUtil.memFree(buffer2);
+		
+		/* ===========
+		 *  MAIN LOOP
+		 * =========== */
+		
+		boolean isRunning = true;
+		while(isRunning)
+		{
+			// Poll all occuring events of the window. This includes keyboard inputs, mouse inputs and window close demands aswell. 
+			GLFW.glfwPollEvents();
+			
+			// There are usually two buffers. one front and one back buffer. If you are rendering to the screen you are always rendering
+			// to the back buffer. With this you are switching the place of both buffers to show the scene you just rendered. 
+			GLFW.glfwSwapBuffers(windowId);
+			
+			// If the "ESC"-key got pressed the demo program should close.
+			if(GLFW.glfwGetKey(windowId, GLFW.GLFW_KEY_ESCAPE) == GLFW.GLFW_PRESS) isRunning = false;
+			
+			// If the window is demanded of closing by pressing the "X" icon the demo program should close.
+			if(GLFW.glfwWindowShouldClose(windowId)) isRunning = false;
+
+			/* =============
+			 *  UPDATE CODE
+			 * =============*/
+			
+			// For this demo there is no update code.
+			
+			/* ====================
+			 * RENDER CODE
+			 * ==================== */
+			
+			// Set the viewport for rendering to the dimensions of the window.
+			// This is not neccessary as long as the only framebuffer been rendered to is the
+			// framebuffer of the window itself. The moment you are rendering to multiple framebuffers
+			// you have to set the viewport to the corresponding dimensions of the framebuffer you're gonna
+			// rendering to.
+			GL11.glViewport(0, 0, WIDTH, HEIGHT);
+			
+			// Setting the default color of the pixels that are not affected by the rendered objects.
+			GL11.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+			
+			// Clearing the pixeldata, the depthdata and the stencildata of the screen.
+			GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT | GL11.GL_STENCIL_BUFFER_BIT);
+			
+			// Binding the VAO that contains the vertex data of our triangle.
+			GL30.glBindVertexArray(triangleVAOId);
+			
+			// Enabling the position pointer "0" for streaming the vertex data of the triangle to gpu.
+			GL20.glEnableVertexAttribArray(0);
+			
+			// Enabling the position pointer "1" for streaming the texture coordinate data of the triangle to gpu.
+			GL20.glEnableVertexAttribArray(1);
+			
+			// Starting the shader.
+			GL20.glUseProgram(shaderProgramId);
+			
+			GL13.glActiveTexture(GL13.GL_TEXTURE0);
+			GL11.glBindTexture(GL30.GL_TEXTURE_2D, textureId1);
+			GL20.glUniform1i(textureUniformLocation, 0);
+			
+			// Drawing the triangle.
+			GL11.glDrawArrays(GL11.GL_TRIANGLE_STRIP, 0, 3);
+			
+			// Enabling blending.
+			GL11.glEnable(GL11.GL_BLEND);
+			
+			// Setting the blending function to f(src,dst) = src * 1 + dst * 1.
+			// Here dst stands for the destination color. This is the color of the current pixel that is already in the framebuffer.
+			// Src stands for the source color. This is the color of the current pixel that is applied by the next shader call.
+			// The result of the function is the new pixel color after a rendering call.
+			GL11.glBlendFunc(GL11.GL_ONE, GL11.GL_ONE);
+			
+			GL13.glActiveTexture(GL13.GL_TEXTURE0);
+			GL11.glBindTexture(GL30.GL_TEXTURE_2D, textureId2);
+			GL20.glUniform1i(textureUniformLocation, 0);
+			
+			// Drawing the triangle.
+			GL11.glDrawArrays(GL11.GL_TRIANGLE_STRIP, 0, 3);
+			
+			// Disabling blending.
+			GL11.glDisable(GL11.GL_BLEND);
+			
+			// Stopping the running shader.
+			GL20.glUseProgram(0);
+		}
+		
+		/* =====================
+		 * FREE RESOURCES
+		 * ===================== */
+		
+		GL30.glDeleteVertexArrays(triangleVAOId);
+		GL30.glDeleteBuffers(triangleVertexVBOId);
+		
+		// Deleting the shader program and the vertex and fragment shader.
+		// Deleting the shader program does not automatically delete the attached shaders,
+		// as they can be attached to multiple shader programs.
+		GL20.glDeleteProgram(shaderProgramId);
+		GL20.glDeleteShader(vertexShaderId);
+		GL20.glDeleteShader(fragmentShaderId);
+		
+		GL11.glDeleteTextures(textureId1);
+		GL11.glDeleteTextures(textureId2);
+		
+		GLFW.glfwDestroyWindow(windowId);
+	}
+}
